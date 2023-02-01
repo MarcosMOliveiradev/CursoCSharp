d8bb81333d948b8494199998b781ac1b96f3acbc
Marcos Suporte TI <suporte01@WENG02.INFO>
2023-02-01 16:55:03 -0300

    Primeiras aulas de CSharp

---
+++ aula 02/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int idade = 25;
+        string nome = "Marcos";
+
+        Console.WriteLine(idade);
+    }
+}
---
+++ aula 03/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("Escreva um numero");
+        double numero_1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Escreva o segundo numero");
+        double numero_2 = Convert.ToDouble(Console.ReadLine());
+
+        double total = numero_1 + numero_2;
+
+        double salario = 3000.13;
+
+        Console.WriteLine("O total da soma é " + total);
+
+        Console.WriteLine("Tecle enter para sair");
+        Console.ReadLine();
+    }
+}
