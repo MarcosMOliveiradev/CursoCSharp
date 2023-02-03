172e176eb5fdb9d150294f3a53b8714672fefdc6
Marcos Suporte TI <suporte01@WENG02.INFO>
2023-02-03 13:36:18 -0300

    novas aulas

---
+++ aula 05 - laco/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Laço de repetição - while");
+
+        double investimento = 1000;
+        double juros = 0.005;
+        int mes = 1;
+
+        // laço while
+
+       /* while(mes <= 12)
+        {
+            investimento = investimento + investimento * juros;
+            Console.WriteLine("Seu rendimento no mês " + mes + " foi de R$ " + investimento);
+
+            mes++;
+        }*/
+
+
+        // laço fo
+        for(int i = 1; i <= 12; i++)
+        {
+            investimento = investimento + investimento * juros;
+            Console.WriteLine("Seu rendimento no mês " + i + " foi de R$ " + investimento);
+        }
+
+
+
+        Console.WriteLine("Tecle Enter para sair");
+        Console.ReadLine();
+    }
+}
---
+++ aula04 - se/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int numero = 10;
+
+        if(numero == 10)
+        {
+            Console.WriteLine("verdade o numero é 10");
+        } else
+        {
+            Console.WriteLine("O numero não é 10");
+        }
+    }
+}
